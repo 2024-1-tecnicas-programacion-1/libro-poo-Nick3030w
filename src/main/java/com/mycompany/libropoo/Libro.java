b77bad21ab3d5e7c1609df721ad98a3cb9742ba8
Nick3030w <niconianessu@gmail.com>
2024-04-22 09:00:37 -0500

    Se completo el ejercicio

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,61 @@
 package com.mycompany.libropoo;
 
 public class Libro {
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int anoPublicacion;
+    private boolean leido;
+    /**
+     * Contructor de la clase
+     * complejidad temporal: 0(1) complejidad constante
+     * @param titulo
+     * @param autor
+     * @param genero
+     * @param anoPublicacion
+     * @param leido 
+     */
+    public Libro(String titulo, String autor, String genero, int anoPublicacion, Boolean leido){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.anoPublicacion = anoPublicacion;
+        this.leido = leido;
+    }
+    /**
+     * metodo que muestra la informacion del libro
+     * complejidad temporal: 0(1) complejidad constante
+     * @return 
+     */
+    public String mostrarInfo(){
+        return "Titulo: " +titulo + ", Autor: " + autor + ", Año de publicación: " + anoPublicacion + ", género: " + genero;
+    }
+    /**
+     * metodo que marca el libro como leido
+     * complejidad temporal: 0(1) complejidad constante
+     * @return 
+     */
+    public void marcarLeido(){
+        this.leido=true;
+    }
+    /**
+     * metodo que me dice si el libro es antiguo
+     * complejidad temporal: 0(1) complejidad constante
+     * @param anoPublicacion
+     * @return 
+     */
+    public boolean esAntiguo(int anoPublicacion){
+        if(anoPublicacion<1974){
+            return true;
+        }
+    return false;
+    }
+
+    public boolean isLeido() {
+        return leido;
+    }
     
-}
+    
+    
+    
+    }
--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,25 @@
 package com.mycompany.libropoo;
-
+import java.util.Scanner;
+/**
+ * Clase principal
+ * @author USUARIO
+ */
 public class Principal {
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        String titulo = lector.next();
+        String autor = lector.next();
+        String genero = lector.next();
+        int anoPublicacion = lector.nextInt();
+        Boolean leido = false;
+        Libro libro = new Libro(titulo, autor, genero, anoPublicacion, leido);
+        
+        libro.marcarLeido();
+        System.out.println(libro.mostrarInfo());
+        System.out.println(libro.isLeido());
+        libro.marcarLeido();
+        System.out.println(libro.isLeido());
+        System.out.println(libro.esAntiguo(anoPublicacion));
+    }
     
 }
